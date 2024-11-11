e9de5c98e9554ffa3626ce47bd3e807f3613128a
Joe <joehewett1@gmail.com>
2024-11-10 21:49:01 -0800

    add metrics tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <script defer src="https://umami-production-fb91.up.railway.app/script.js" data-website-id="d815d931-f0bd-42fe-8895-8469b7c13912"></script>
       </Head>
       <body>
         <Main />
